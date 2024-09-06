8e21fe3bf5c604a2351bb971da686c2643336de5
Sam <79186820+1narracs@users.noreply.github.com>
2024-09-06 12:34:56 +1000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.spotify = {};
 config.lastfm = {};
 
 config.spotify.clientId = '3c6d8b939c304b23b0dc359844f3ba6d';
-config.spotify.clientSecret = '5dce67f7537f49ac852f5f0b7ebc29a3';
+config.spotify.clientSecret = '';
 config.lastfm.apiKey = '4ba402280904c0162f5a6d4b9ffba1f2';
 
 module.exports = config;
